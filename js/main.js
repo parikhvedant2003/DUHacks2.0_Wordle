03767da532cb059cbb537c362990f44b6f598335
Veddant Parikh <parikhvedant2003@gmail.com>
2023-02-19 13:42:30 +0530

    Second Commit

--- js/main.js
+++ js/main.js
@@ -1,18 +1,74 @@
 document.addEventListener("DOMContentLoaded", () => 
     {
         createSquares()
-        const keys = document.querySelectorAll('keyboard-row button')
-        const GuessedWords = [[]]
-        for (let i = 0; i < keys.length; i++)
+        const keys = document.querySelectorAll('.keyboard-row button')
+        let guessedWords = [[]]
+        let availableSpace = 1
+        let word = "dairy"
+        let guessedWordCount = 0;
+        function getCurrentArr()
         {
-            keys[i].onclick = ({target}) =>
-            {
-                const key = target.getAttribute('data-key');
-                console.log(key)
-            }
+            const numberOfGuessedWords = guessedWords.length
+            return guessedWords[numberOfGuessedWords - 1]
         }
         function updateGuessedWords(letter)
         {
+            const currentWordArr = getCurrentArr()
+            if (currentWordArr && currentWordArr.length < 5)
+            {
+                currentWordArr.push(letter)
+                const availableSpaceEl = document.getElementById(String(availableSpace))
+                availableSpace += 1
+                availableSpaceEl.textContent = letter
+            }
+
+        }
+        function getTileColor(letter, index)
+        {
+            const isCorrectLetter = word.includes(letter)
+            if (!isCorrectLetter)
+            {
+                return "rgb(58, 58, 60)";
+            }
+            const letterInThatPosition = word.charAt(index)
+            const isCorrectPosition = letter === letterInThatPosition
+            if (isCorrectPosition)
+            {
+                return "rgb(83, 141, 78)"
+            }
+            return "rgb(181, 159, 59)"
+        }
+        function handleSubmitWord()
+        {
+            const currentWordArr = getCurrentArr()
+            if (currentWordArr.length !== 5)
+            {
+                window.alert("Word must be 5 letters")
+            }
+            const currentWord = currentWordArr.join("")
+            const firstLetterId = guessedWordCount * 5 + 1
+            const interval = 200;
+            currentWordArr.forEach((letter, index) =>
+            {
+                setTimeout(() =>
+                {
+                    const tileColor = getTileColor(letter, index);
+                    const letterId = firstLetterId + index;
+                    const letterEl = document.getElementById(letterId);
+                    letterEl.classList.add("animate_flipInx");
+                    letterEl.style = `background-color:${tileColor}; border-color:${tileColor}`;
+                }, interval * index)
+            })
+            guessedWordCount += 1
+            if (currentWord === word)
+            {
+                window.alert("Congratulations!")
+            }
+            if (guessedWords.length === 6)
+            {
+                window.alert(`Sorry! You have no more guesses, The word is ${word.toUpperCase()}.`)
+            }
+            guessedWords.push([])
 
         }
         function createSquares()
@@ -22,9 +78,25 @@ document.addEventListener("DOMContentLoaded", () =>
             {
                 let square = document.createElement("div")
                 square.classList.add("square")
+                square.classList.add("animate_animated")
                 square.setAttribute("id", index + 1)
                 gameBoard.appendChild(square)
             }
         }
-
+        for (let i = 0; i < keys.length; i++)
+        {
+            keys[i].onclick = ({target}) =>
+            {
+                const letter = target.getAttribute("data-key");
+                
+                if (letter === 'enter')
+                {
+                    handleSubmitWord()
+                    return
+                }
+                updateGuessedWords(letter)
+                // console.log(letter)
+                
+            };
+        }
     })
